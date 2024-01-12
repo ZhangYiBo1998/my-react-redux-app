8dac4c0af0b911db7cbf8a9f40d464980117b091
ZhangYiBo1998 <27582180@qq.com>
2024-01-12 17:25:54 +0800

    更新注释

--- src/redux/counter/counterAPI.js
+++
@@ -1,7 +0,0 @@
-// A mock function to mimic making an async request for data
-// 模拟对数据进行异步请求的mock函数
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
-}
